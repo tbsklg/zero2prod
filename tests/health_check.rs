e019481a7cf164d33027f3c312983a2dc9c51274
Tobias Klug <tbs.klg@gmail.com>
2024-01-22 06:45:11 +0100

    Provide integration test for health check

---
+++ tests/health_check.rs
@@ -0,0 +1,21 @@
+#[tokio::test]
+async fn health_check_works() {
+    spawn_app();
+
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert!(response.status().is_success());
+    assert_eq!(response.content_length(), Some(0));
+}
+
+fn spawn_app() {
+    let server = zero2prod::run().expect("Failed to bind address");
+
+    let _ = tokio::spawn(server);
+}
