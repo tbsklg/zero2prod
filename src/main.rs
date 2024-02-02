7664ae75f3f670ccd8bd970fae753ab9f03329ec
Tobias Klug <tbs.klg@gmail.com>
2024-02-02 13:04:43 +0100

    Adjust package structure for configuration

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use std::net::TcpListener;
 
-use zero2prod::run;
+use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind address");
-
     run(listener)?.await
 }
